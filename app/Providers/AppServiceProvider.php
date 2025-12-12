aa4fef29c4ce7c0a3f9aa36517427cde2752c855
romain <romain.wgr9@gmail.com>
2025-12-12 20:05:01 +0100

    test brevo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
+use Symfony\Component\Mailer\Transport\Dsn;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +29,14 @@ class AppServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
         return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
     });
+    Mail::extend('brevo', function (array $config = []) {
+            return (new BrevoTransportFactory())->create(
+                new Dsn(
+                    'brevo+api', 
+                    'default',
+                    $config['key'] 
+                )
+            );
+        });
     }
 }
